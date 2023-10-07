6257f225a5939204813224a7babaf293cc757c3d
huyixi <ihuyixi@gmail.com>
2023-10-07 21:32:26 +0800

    feat: add scrollToTopBtn

--- themes/blue/assets/js/custom.js
+++ themes/blue/assets/js/custom.js
@@ -1,39 +1,65 @@
 // tempBanner
 document.addEventListener("DOMContentLoaded", function () {
-    var banner = document.getElementById("tempBanner");
+  var banner = document.getElementById("tempBanner");
 
-    if ((window.location.pathname === '/zh/' || window.location.pathname === '/en/') && !localStorage.getItem('bannerDisplayed')) {
-        banner.style.display = "block";
-        localStorage.setItem('bannerDisplayed', 'true');
+  if (
+    (window.location.pathname === "/zh/" || window.location.pathname === "/en/") &&
+    !localStorage.getItem("bannerDisplayed")
+  ) {
+    banner.style.display = "block";
+    localStorage.setItem("bannerDisplayed", "true");
 
-        setTimeout(function() {
-            banner.style.transform = "scaleY(0)";
-            setTimeout(function() {
-                banner.style.display = "none";
-            }, 500);
-        }, 3000);
-    }
+    setTimeout(function () {
+      banner.style.transform = "scaleY(0)";
+      setTimeout(function () {
+        banner.style.display = "none";
+      }, 500);
+    }, 3000);
+  }
 });
+
+// scrollToTop
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var scrollToTopBtn = document.getElementById("scrollToTop");
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+document.getElementById("scrollToTop").onclick = function () {
+  topFunction();
+};
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
 
 // like hardware
 var modal = document.getElementById("likeModal");
 var span = document.getElementsByClassName("close")[0];
 
 if (span) {
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
 }
 
 var showModals = document.getElementsByClassName("show-modal");
 for (var i = 0; i < showModals.length; i++) {
-    showModals[i].onclick = function() {
-        modal.style.display = (modal.style.display === "block") ? "none" : "block";
-    }
+  showModals[i].onclick = function () {
+    modal.style.display = modal.style.display === "block" ? "none" : "block";
+  };
 }
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
