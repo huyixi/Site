dbfe04ff36ab84e7c30b70b58c861dc83b6ee15e
huyixi <ihuyixi@gmail.com>
2023-10-18 09:46:36 +0800

    test lastmod

---
+++ content/posts/testlastmod/index.md
@@ -0,0 +1,9 @@
+---
+title: "Testlastmod"
+subtitle: ""
+date: 2023-10-18T09:32:59+08:00
+draft: false
+tags: []
+category: [Blog]
+summary: 
+---
---
+++ utils/update_lastmod.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 获取所有更改的 Markdown 文件
+CHANGED_FILES=$(git diff --cached --name-only --diff-filter=AM | grep '\.md$')
+
+CURRENT_DATE=$(date +"%Y-%m-%d")
+
+# 遍历所有更改的 Markdown 文件
+for FILE in $CHANGED_FILES; do
+    # 检查文件中是否存在 lastmod 参数
+    if grep -q "^lastmod:" $FILE; then
+        # 更新 lastmod 日期
+        sed -i "/^lastmod:/c\lastmod: $CURRENT_DATE" $FILE
+    else
+        # 否则，在文件的前置参数部分添加 lastmod 参数
+        sed -i "/^---/a\lastmod: $CURRENT_DATE" $FILE
+    fi
+done
