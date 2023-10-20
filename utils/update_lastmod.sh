7f7042a52d4a86402df3da9848f3aade83a45a5e
huyixi <ihuyixi@gmail.com>
2023-10-20 09:55:22 +0800

    Update update_lastmod.sh

--- utils/update_lastmod.sh
+++ utils/update_lastmod.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
-
-# 获取所有更改的 Markdown 文件
 CHANGED_FILES=$(git diff --cached --name-only --diff-filter=AM content/ | grep '\.md$')
 
 CURRENT_DATE=$(date +"%Y-%m-%d")
 
-# 遍历所有更改的 Markdown 文件
 for FILE in $CHANGED_FILES; do
-    # 检查文件中是否存在 lastmod 参数
+    if git diff --cached "$FILE" | grep -q "^+lastmod:"; then
+        echo "Skipping $FILE as lastmod has been manually modified."
+        continue
+    fi
+    
     if grep -q "^lastmod:" $FILE; then
-        # 更新 lastmod 日期
         sed -i '' -e "/^lastmod:/c\\
 lastmod: $CURRENT_DATE" $FILE
     else
-        # 否则，在文件的前置参数部分添加 lastmod 参数
         sed -i '' -e "/^---/a\\
 lastmod: $CURRENT_DATE" $FILE
     fi
