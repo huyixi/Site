75b602cc3f5b455acfaba5b5d022573725909a02
huyixi <ihuyixi@gmail.com>
2023-11-06 23:32:29 +0800

    u

---
+++ content/posts/Weekly-28/index.md
@@ -0,0 +1,31 @@
+---
+title: "Weekly 28"
+subtitle: ""
+date: 2023-11-05T17:19:01+08:00
+lastmod: 2023-11-05T17:19:01+08:00
+draft: false
+tags: []
+category: [Weekly]
+---
+
+## 文章
+
+## 日记
+
+#### Sun Nov 5 17:19, 2023
+
+好看的 About 页面：
+
+- https://blog.zhheo.com/about/
+
+- https://www.snhere.com/about-sn/
+- https://www.anzifan.com/
+- https://www.anzifan.com/me
+
+#### Mon Nov 6 22:09, 2023
+
+早上去测了一下度数，疯涨。后续也得控制一下连续看屏幕的时间。
+
+今天工作状态有点差，可能是因为最近几天睡眠时间比较少，也有可能因为考试后遗症。
+
+需要调整一下，闲杂事尽量在状态差的时候都处理掉。
--- themes/blue/assets/js/custom.js
+++ themes/blue/assets/js/custom.js
@@ -5,15 +5,16 @@ function handleImageError(imageElement) {
 }
 
 function showBanner() {
+  console.log("showBanner");
   var banner = document.getElementById("tempBanner");
   if (banner) {
     banner.style.display = "block";
     localStorage.setItem("bannerDisplayed", "true");
     setTimeout(function () {
-      banner.style.transform = "scaleY(0)"; // 缩小横幅
+      banner.style.transform = "scaleY(0)";
       setTimeout(function () {
         banner.style.display = "none";
-      }, 500); // 0.5秒后隐藏横幅
+      }, 500);
     }, 3000);
   }
 }
@@ -21,39 +22,6 @@ function showBanner() {
 document.addEventListener("DOMContentLoaded", function () {
   // tempBanner
   setTimeout(function () {
-    // Select all images on the page
-    var images = document.querySelectorAll("img");
-    var banner = document.getElementById("tempBanner");
-
-    // // Function to show the banner
-    // var showBanner = function () {
-    //   if (banner && !localStorage.getItem("bannerDisplayed")) {
-    //     banner.style.display = "block";
-
-    //     // Set the bannerDisplayed flag in localStorage
-    //     localStorage.setItem("bannerDisplayed", "true");
-
-    //     // After 3 seconds, start the hide animation
-    //     setTimeout(function () {
-    //       banner.style.transform = "scaleY(0)"; // Shrink the banner
-
-    //       // After 500 milliseconds or 0.5 seconds, which matches the transition duration, hide the banner
-    //       setTimeout(function () {
-    //         banner.style.display = "none";
-    //       }, 500);
-    //     }, 3000);
-    //   }
-    // };
-
-    // // Add an error event listener to each image
-    // images.forEach(function (img) {
-    //   img.addEventListener("error", function () {
-    //     console.log("Failed to load image:", img.src);
-    //     banner.style.display = "block";
-    //   });
-    // });
-
-    // Additional logic can be placed here (e.g., checking the pathname)
     if (
       (window.location.pathname === "/zh/" || window.location.pathname === "/en/") &&
       !localStorage.getItem("bannerDisplayed")
@@ -190,3 +158,18 @@ document.addEventListener("DOMContentLoaded", function () {
   observer.observe(article);
   headings.forEach((heading) => observer.observe(heading));
 });
+
+window.onload = function () {
+  // if images are not loaded, show banner
+  // Select all images on the page
+  var images = document.querySelectorAll("img");
+
+  // Add an error event listener to each image
+  images.forEach(function (img) {
+    img.addEventListener("error", function () {
+      console.log("Failed to load image:", img.src);
+      showBanner();
+    });
+  });
+  console.log("所有资源都已加载完成");
+};
